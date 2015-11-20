a52dda71ed7bf6f921964af4f2496fdca4382a8a
lkhedlund <lkhedlund@gmail.com>
2015-11-20 16:45:14 +0000

    Stable before adding a create group tile'

--- app/actions.rb
+++ app/actions.rb
@@ -7,8 +7,9 @@ helpers do
 
 	def current_user
 		@current_user = User.find(2)
-    # @current_user = User.find(session[:user_id]) if session[:user_id]
+    #@current_user = User.find(session[:user_id]) if session[:user_id]
 	end
+
 end
 
 before do
