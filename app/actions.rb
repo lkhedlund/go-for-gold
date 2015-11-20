ce9622521564df63237febfed05f450481185161
lkhedlund <lkhedlund@gmail.com>
2015-11-20 04:15:49 +0000

    Shows current user's groups on the main page

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,21 @@
 # Homepage (Root path)
+helpers do
+	def handle_errors
+		@errors = session[:error] if session[:error]
+		session[:error] = nil
+	end
+
+	def current_user
+		@current_user = User.find(2)
+    # @current_user = User.find(session[:user_id]) if session[:user_id]
+	end
+end
+
+before do
+	current_user
+	handle_errors
+end
+
 get '/' do
   erb :index
 end
