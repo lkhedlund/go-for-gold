feb6b8161c23d95bf1a90821f8b32cc653c86520
lkhedlund <lkhedlund@gmail.com>
2015-11-20 20:27:46 +0000

    Stable working version. No linking between pages

--- app/actions/actions_lars.rb
+++ app/actions/actions_lars.rb
@@ -1,31 +1,3 @@
-# Homepage (Root path)
-helpers do
-	def handle_errors
-		@errors = session[:error] if session[:error]
-		session[:error] = nil
-	end
-
-	def current_user
-		@current_user = User.find(2)
-    #@current_user = User.find(session[:user_id]) if session[:user_id]
-	end
-
-	def group_users
-		session[:group_users] = []
-	end
-
-end
-
-before do
-	current_user
-	handle_errors
-	group_users
-end
-
-get '/' do
-  erb :index
-end
-
 get '/results' do
   @groups = Group.all
   unless params[:search_term].blank?
