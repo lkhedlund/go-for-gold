f4e0118d1cc7dc570f512435a0de814f58cbe086
Brittany See <brittanyjsee@gmail.com>
2015-11-21 00:36:27 +0000

    edit form fix

--- app/actions/actions_brittany.rb
+++ app/actions/actions_brittany.rb
@@ -1,14 +1,24 @@
-put '/groups/:group_id/activities/:id' do |id|
+put '/groups/:group_id/activities/:id' do |group_id, id|
   @group_id = params[:group_id]
   @activity = Activity.find(id)
-  @activity.update_attributes(params[:@activity])
-  redirect "/groups/:group_id/activities/:id"
-end
+  @activity.update_attributes(
+  name: params[:name],
+  time: params[:time],
+  location: params[:location],
+  completed: false,
+  group_id: params[:group_id]
+  )
+  if @activity.save
+    redirect "/groups/#{@group_id}/activities/#{id}"
+  else
+    erb :'/groups/activities/edit'
+  end
+end  
 
 get '/groups/:group_id/activities/new' do
   @group_id = params[:group_id]
   @activity = Activity.new
-  erb :'groups/activities/new'
+  erb :'/groups/activities/new'
 end
 
 post '/groups/:group_id' do
@@ -23,19 +33,19 @@ post '/groups/:group_id' do
   if @activity.save
     redirect "/groups/#{@group_id}"
   else
-    erb :'groups/activities/new'
+    erb :'/groups/activities/new'
   end
 end
 
 get '/groups/:group_id/activities/:id' do
   @group_id = params[:group_id]
   @activity = Activity.find params[:id]
-  erb :'groups/activities/show'
+  erb :'/groups/activities/show'
 end
 
 get '/groups/:group_id/activities/:id/edit' do
   @group_id = params[:group_id]
   @activity = Activity.find params[:id]
-  erb :'groups/activities/edit'
+  erb :'/groups/activities/edit'
 end
 
